b61ee8260ba265b3d3811d61b06f737b947da615
tdamrob <damir.robic@ericssonnikolatesla.com>
2024-10-29 07:43:48 +0100

    asdasd

--- src/hr/learning/daro/general/LogiranjeSvihVrsta.java
+++ src/hr/learning/daro/general/LogiranjeSvihVrsta.java
@@ -2,7 +2,7 @@ package hr.learning.daro.general;
 
 public class LogiranjeSvihVrsta {
 	public static void main(String[] args) {
-		System.out.println("Logiranje na standardnom outputu");
+		System.out.println("Logiranje na standardnom outputu.");
 	}
 
 }
